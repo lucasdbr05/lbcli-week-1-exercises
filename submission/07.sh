b1bd6950e0c54a86b376312c4ef763d0f4239bae
lucasdbr05 <lucasgabrieljake@gmail.com>
2026-03-17 17:18:27 -0300

    solve exercise 07

--- submission/07.sh
+++ submission/07.sh
@@ -1,1 +1,9 @@
 # Generate a P2SH address.
+
+A1=$(bitcoin-cli -regtest getnewaddress)
+PUB1=$(bitcoin-cli -regtest getaddressinfo "$A1" | jq -r '.pubkey')
+
+A2=$(bitcoin-cli -regtest getnewaddress)
+PUB2=$(bitcoin-cli -regtest getaddressinfo "$A2" | jq -r '.pubkey')
+
+bitcoin-cli -regtest createmultisig 2 "[\"$PUB1\",\"$PUB2\"]" | jq -r '.address'
