d8b7e8ea0efa9bc7ed73f5e775aa2441578735f5
lucasdbr05 <lucasgabrieljake@gmail.com>
2026-03-18 14:29:34 -0300

    fix 05

--- submission/05.sh
+++ submission/05.sh
@@ -1,5 +1,5 @@
 # Check the total amount in the wallet.
 
 ADDR=$(bitcoin-cli -regtest -rpcwallet=builderswallet getnewaddress)
-bitcoin-cli -regtest generatetoaddress 101 "$ADDR"
+bitcoin-cli -regtest generatetoaddress 101 "$ADDR" > /dev/null
 bitcoin-cli -regtest -rpcwallet=builderswallet getbalance
